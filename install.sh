a5430c38b3c786594c831f206768158cd4eed88c
Elvin Kim <32478597+sudosubin@users.noreply.github.com>
2021-07-11 01:01:21 +0900

    Use local permission only (#11)
    * Fix to use local installed zpl-open
    
    * Fix installation script
    
    * Fix xdg-mime default desktop file path
    
    * Fix file name in test case
    
    * Update README.md for changes

--- install.sh
+++ install.sh
@@ -4,22 +4,16 @@ install_zpl_opener() {
   local root_dir;
   root_dir="$(dirname "${BASH_SOURCE[0]}")";
 
-  # make script executable
-  chmod +x "$root_dir/src/zpl-open.sh";
-
-  # remove pre-installed files
-  sudo rm -f "/usr/local/lib/zpl-open.sh";
-  rm -f "$HOME/.local/share/applications/zpl-opener.desktop";
-
-  # copy files to local
+  # copy files
+  mkdir -p "$HOME/bin"
   mkdir -p "$HOME/.local/share/applications";
-  sudo cp "$root_dir/src/zpl-open.sh" "/usr/local/lib/zpl-open.sh";
+
+  cp "$root_dir/src/zpl-open" "$HOME/bin/zpl-open";
   cp "$root_dir/src/zpl-opener.desktop" \
     "$HOME/.local/share/applications/zpl-opener.desktop";
 
   # register opener
-  xdg-mime default "$HOME/.local/share/applications/zpl-opener.desktop" \
-    "x-scheme-handler/zpl";
+  xdg-mime default "zpl-opener.desktop" "x-scheme-handler/zpl";
 
   echo "installation complete!";
 }
--- tests/shared/opener.bash
+++ tests/shared/opener.bash
@@ -2,5 +2,5 @@
 
 opener() {
   # do not open browser, just return web url string
-  bash "$BATS_TEST_DIRNAME/../src/zpl-open.sh" "--headless" "$*";
+  bash "$BATS_TEST_DIRNAME/../src/zpl-open" "--headless" "$*";
 }
